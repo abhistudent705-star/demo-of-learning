1016f4dc4d1fc5de683cbbbbe3aaa73778bda7cf
abhistudent705-star <abhistudent705@gmail.com>
2026-07-27 05:03:15 +0100

    area of circle

--- calculator1.cpp
+++ calculator1.cpp
@@ -1,17 +1,37 @@
 #include <iostream>
+// using namespace std;
+// // AREA OF RECTAGLE  C:\Users\Abhir\OneDrive\Documents\git-demo\demo-of-learning\calculator1.cpp
+// int main()
+// {
+
+//     int length, breadth, area;
+
+//     cout << "Enter the length and breadth of the rectangle :";
+//     cin >> length >> breadth;
+
+//     area = length * breadth;
+
+//     cout << " The area of the rectangle is : " << area;
+
+//     return 0;
+// }
+
+// AREA OF CIRCLE
+
 using namespace std;
-// AREA OF RECTAGLE  C:\Users\Abhir\OneDrive\Documents\git-demo\demo-of-learning\calculator1.cpp
+
 int main()
 {
 
-    int length, breadth, area;
+    float radius, pi = 3.14, area;
 
-    cout << "Enter the length and breadth of the rectangle :";
-    cin >> length >> breadth;
+    cout << "Enter the radius of the circle";
 
-    area = length * breadth;
+    cin >> radius;
 
-    cout << " The area of the rectangle is : " << area;
+    area = pi * radius * radius;
+
+    cout << " The area of the circle is =" << area;
 
     return 0;
 }
