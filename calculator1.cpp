6b7195ef8e629b5ea5f39a4a265fb004802a1833
abhistudent705-star <abhistudent705@gmail.com>
2026-07-27 02:58:20 +0100

    area of rectangle

---
+++ calculator1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+// AREA OF RECTAGLE  C:\Users\Abhir\OneDrive\Documents\git-demo\demo-of-learning\calculator1.cpp
+int main()
+{
+
+    int length, breadth, area;
+
+    cout << "Enter the length and breadth of the rectangle :";
+    cin >> length >> breadth;
+
+    area = length * breadth;
+
+    cout << " The area of the rectangle is : " << area;
+
+    return 0;
+}
