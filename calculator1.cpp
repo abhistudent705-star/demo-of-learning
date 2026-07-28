ce85a2a6f1c7b339c3b839e14e8cd0436cd7797f
abhistudent705-star <abhistudent705@gmail.com>
2026-07-28 21:50:45 +0100

    sum of n number of nutural number

--- calculator1.cpp
+++ calculator1.cpp
@@ -1,37 +1,23 @@
 #include <iostream>
-// using namespace std;
-// // AREA OF RECTAGLE  C:\Users\Abhir\OneDrive\Documents\git-demo\demo-of-learning\calculator1.cpp
-// int main()
-// {
-
-//     int length, breadth, area;
-
-//     cout << "Enter the length and breadth of the rectangle :";
-//     cin >> length >> breadth;
-
-//     area = length * breadth;
-
-//     cout << " The area of the rectangle is : " << area;
-
-//     return 0;
-// }
-
-// AREA OF CIRCLE
 
 using namespace std;
 
 int main()
 {
 
-    float radius, pi = 3.14, area;
+    int n;
+    int sum = 0;
 
-    cout << "Enter the radius of the circle";
+    cout << "Enter the n number for natural number for sum";
+    cin >> n;
 
-    cin >> radius;
+    for (int i = 0; i <= n; i++)
+    {
 
-    area = pi * radius * radius;
-
-    cout << " The area of the circle is =" << area;
+        sum = sum + i;
+        cout << " sum by the step" << sum << endl;
+    }
+    cout << "sum of the first number of naruarl number" << sum << endl;
 
     return 0;
 }
