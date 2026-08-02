f70498819f8aa5d22ac74d477eb9ece1413c3f3c
abhistudent705-star <abhistudent705@gmail.com>
2026-08-02 22:11:19 +0100

    Array of descending numbers

--- calculator1.cpp
+++ calculator1.cpp
@@ -16,7 +16,7 @@ int main()
     {
         for (int j = i + 1; j < n; j++)
         {
-            if (arr[i] > arr[j])
+            if (arr[i] < arr[j])
             {
 
                 temp = arr[i];
